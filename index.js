613cd4d45f4419b15eee6b6df6e5efc338b1de6b
Chris Kozlowski <kozlowskicd@gmail.com>
2019-06-17 16:29:12 -0700

    moved q-server

--- index.js
+++ index.js
@@ -11,20 +11,4 @@ const mongooseOptions = {
 
 mongoose.connect(process.env.MONGODB_URI, mongooseOptions);
 
-const QServer = require('@nmq/q/server');
-
-QServer.start();
-
-const files = new QServer('files');
-files.monitorEvent('file-save');
-files.monitorEvent('file-error');
-
-const database = new QServer('database');
-
-database.monitorEvent('create');
-database.monitorEvent('read');
-database.monitorEvent('update');
-database.monitorEvent('delete');
-database.monitorEvent('error');
-
 require('./src/app.js').start(process.env.API_PORT || 8080);
